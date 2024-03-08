30f5dd972b19cbe7c4196f03eee81c1652c958e0
Muhammad Syulhan <janmuno77@gmail.com>
2024-03-08 12:43:00 +0800

    new

---
+++ Case1-MobileAppPembayaranQRIS/Case1-MobileAppPembayaranQRIS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Case1-MobileAppPembayaranQRIS
+//
+//  Created by M. Syulhan Al Ghofany on 08/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
